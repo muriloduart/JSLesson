387c51ee2f2206282ed980c31d36cb2e78b0e90c
muriloduart <muriloduarte@outlook.com>
2022-11-13 13:50:59 -0400

    Learning Atribuicao

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let b = 3
+
+console.log(b) // valor de b simples 
+
+b += a   //acrescenta valor de 'a' ao 'b' | b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
