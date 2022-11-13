3a5ec2b29038b97950d623a6d27627e2eceee81c
muriloduart <muriloduarte@outlook.com>
2022-11-13 18:25:34 -0400

    Learning Operadores aritimeticos

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,17 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+
+const soma = a + b + c + d
+console.log(soma)
+
+const sub = a - b - c - d
+console.log(sub)
+
+const multiplicacao = a * b
+console.log(multiplicacao)
+
+const divisao = d / b
+console.log(divisao)
+
+const modulo =  b % a // esse seria o resto da divisao
+console.log(modulo)
