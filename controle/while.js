69d5ccde65457a5d5a440a894b55d8475773a81c
muriloduart <muriloduarte@outlook.com>
2022-11-14 15:17:07 -0400

    Learning Do/While

--- controle/while.js
+++ controle/while.js
@@ -1,13 +1,14 @@
-function getInteiroAleatorioEntre(min, max){
+function getInteiroAleatorioEntre(min, max) {
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
-
-let opcao = -1
-
-while(opcao != -1) {
+ 
+let opcao = 0
+ 
+while (opcao != -1) {
     opcao = getInteiroAleatorioEntre(-1, 10)
-    console.log(`Opcão escolhida foi ${opcao} .`)
+    // console.log(`Opção escolhida foi ${opcao}.`)
+    console.log('Opção escolhida foi ' + opcao + '.')
 }
-
+ 
 console.log('Até a próxima')
