3942d35f8d0562137e28ad9c8f16cf43043693f6
muriloduart <muriloduarte@outlook.com>
2022-11-14 08:06:39 -0400

    Learning Operadores Ternários

---
+++ fundamentos/ternario.js
@@ -0,0 +1,3 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' // Estrutura expressao retornando T ou F, dps ?, se fot T ':' se for F
+console.log(resultado(7.1))
+console.log(resultado(6.9))
