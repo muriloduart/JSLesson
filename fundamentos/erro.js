c4d626f8065c843111e9910c8ffb8352648e791a
muriloduart <muriloduarte@outlook.com>
2022-11-14 08:52:01 -0400

    Learning Tratamento de erros

---
+++ fundamentos/erro.js
@@ -0,0 +1,22 @@
+function tratarErroELancar(erro){
+  //  throw new Error("Erro novo")
+ // throw 10
+ //throw true
+ //throw 'mensagem'
+ throw{
+    nome: erro.nome,
+    msg: erro.message,
+    date: new Date
+ }
+}
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+const obj = { name: 'Murilo' } // erro: nome por name
+imprimirNomeGritado(obj)
