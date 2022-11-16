57e3d0d06432960181191701693f7ba7acdb1d12
muriloduart <muriloduarte@outlook.com>
2022-11-16 15:39:39 -0400

    4) Exercicios: 1 ao 5

---
+++ exerciciosJS/03expoentes.js
@@ -0,0 +1,11 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function expoentes (base, expoente) {
+    let resultado = Math.pow(base, expoente)
+    return resultado
+}
+
+console.log(expoentes(2, 2))
+
+
+
---
+++ exerciciosJS/04restoDivisao.js
@@ -0,0 +1,11 @@
+//04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+//e o resto da divisão destes dois valores.
+function resto(dividendo, divisor){
+    console.log("Resultado: " + (dividendo/divisor))
+    console.log("Resto: " + (dividendo % divisor)) 
+}
+
+console.log(resto(3, 2))
+
+
+//CONSEGUI FAZER SOZINHO
