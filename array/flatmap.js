841edc6580bf88993423dd9f59c7997218baf4e8
muriloduart <muriloduarte@outlook.com>
2022-11-21 15:29:19 -0400

    5.9) Obejto:Float Map

---
+++ array/flatmap.js
@@ -0,0 +1,19 @@
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: ' Gustavo',
+        nota: 7.1
+    },{
+        nome: 'Ana', 
+        nota: 8.2
+    },{
+        nome: ' Turma M2',
+        alunos:[{
+            nome: 'Joana',
+            nota: 5.4
+        },{
+            nome: 'Bruna',
+            nota: 5.5
+        }]
+    }]
+}]
