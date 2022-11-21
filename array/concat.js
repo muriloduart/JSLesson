d95dbf2f3e7bba7f56e44554a58148463d5bb6ec
muriloduart <muriloduarte@outlook.com>
2022-11-21 15:11:31 -0400

    5.8) Obejto:Imperativo x Declarativo

---
+++ array/concat.js
@@ -0,0 +1,8 @@
+const filhas = ['Valeska, Maria']
+const filhos = ['Murilo, Marcelo']
+
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
